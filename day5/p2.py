74a329f500f0e9ce3c7d7755da793fac489ca6ae
Josh Heilman <josh.heilman@meredith.com>
2020-12-07 11:47:10 -0600

    Adds day 5 problems

---
+++ day5/p2.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+import sys
+
+MIN_ROW = 0
+MAX_ROW = 127
+MIN_COL = 0
+MAX_COL = 7
+
+with open(f'{sys.path[0]}/day5.txt', 'r') as f:
+    seats = [line.strip() for line in f.readlines()]
+
+seat_ids = []
+for seat in seats:
+    row = -1
+    row_lower = MIN_ROW
+    row_upper = MAX_ROW
+
+    col = -1
+    col_lower = MIN_COL
+    col_upper = MAX_COL
+
+    for loc in seat:
+        if loc == 'F':
+            row_upper = int(((row_upper + row_lower + 1) / 2) - 1)
+            row = row_lower
+        elif loc == 'B':
+            row_lower = int(((row_upper + row_lower + 1) / 2))
+            row = row_upper
+        elif loc == 'L':
+            col_upper = int(((col_upper + col_lower + 1) / 2) - 1)
+            col = col_lower
+        elif loc == 'R':
+            col_lower = int(((col_upper + col_lower + 1) / 2))
+            col = col_upper
+
+    seat_id = (row * 8) + col
+    seat_ids.append(seat_id)
+
+seat_ids = sorted(seat_ids)
+for idx, seat_id in enumerate(seat_ids):
+    if seat_ids[idx + 1] != seat_id + 1:
+        print(f'Answer: {seat_id + 1}')
+        exit()
